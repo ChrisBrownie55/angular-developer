c2fd1ac55d00827f1cc0c4a1d6adfd6d926e644d
Christopher Brown <chbphone55@gmail.com>
2019-02-08 05:35:38 +0000

    timer service

--- src/app/pages/pomodoro/pomodoro.component.ts
+++ src/app/pages/pomodoro/pomodoro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TimerService } from './timer/timer.service';
 
 @Component({
   selector: 'ngx-pomodoro',
@@ -6,10 +7,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pomodoro.component.scss']
 })
 export class PomodoroComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor(private timer: TimerService) {}
+  ngOnInit() {}
 }
---
+++ src/app/pages/pomodoro/timer/timer.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TimerService {
+  public minutes: number = 0;
+  public seconds: number = 0;
+
+  private _playing = false;
+  /* I'm using getters and setters so that I can sub & unsub from the timer */
+  public get playing() {
+    return this._playing;
+  }
+  public set playing(value) {
+    if (value && !this.playing) {
+      this.subscribe();
+    } else if (!value && this.playing) {
+      this.unsubscribe();
+    }
+  }
+
+  public onFinished: Function;
+
+  private timer = interval(1000);
+  private subscription: Subscription;
+  private subscribe = () => {
+    this.subscription = this.timer.subscribe(() => {
+      /* decrements the time */
+      if (this.seconds === 0) {
+        this.seconds = 59;
+        this.minutes--;
+      } else {
+        this.seconds--;
+      }
+    });
+  };
+  private unsubscribe = () => {
+    this.subscription.unsubscribe();
+  };
+}
